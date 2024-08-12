e6138cb09003636e715512c78664a8f9ce6b6e7d
김민성 <gimminseong@User-MackBook-Pro.skbroadband>
2024-08-12 19:14:14 +0900

    REF :: 전체 코드 리펙토링

--- lib/Page/myDiary.dart
+++ lib/Page/myDiary.dart
@@ -6,6 +6,19 @@ class MyDiaryPage extends StatefulWidget {
 }
 
 class _MyDiaryPageState extends State<MyDiaryPage> {
+  final List<dynamic> numbers = [7, 12, 23, 32, 34, " ", " "];
+  final int yyyy = 2024;
+  final int dd = 7;
+
+  int? _selectedDate; // 초기값을 null로 설정
+
+  void _selectDate(int date) {
+    setState(() {
+      // 현재 선택된 날짜가 동일하면 선택 해제, 아니면 새로운 날짜 선택
+      _selectedDate = _selectedDate == date ? null : date;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,58 +26,64 @@ class _MyDiaryPageState extends State<MyDiaryPage> {
       appBar: AppBar(
         backgroundColor: Color(0xFFF3F4F6),
         elevation: 0,
-        toolbarHeight: 0, 
+        toolbarHeight: 0,
       ),
       body: SingleChildScrollView(
-        child: Padding(  
-          padding: const EdgeInsets.all(16.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Color(0xFFF2B720),
-                    child: Text('7', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFF4072AC),
-                    child: Text('12', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFFDE4C0E),
-                    child: Text('23', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFF9195A4),
-                    child: Text('32', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFF9195A4),
-                    child: Text('34', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFFB0B0B0),
-                    child: Icon(Icons.add, color: Colors.white),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFFF2B720),
-                    child: Icon(Icons.add, color: Colors.white),
-                  ),
-                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: numbers.asMap().entries.map((entry) {
+                  int index = entry.key;
+                  dynamic number = entry.value;
+                  Color circleColor;
+
+                  if (index == 0 || index == numbers.length - 1) {
+                    circleColor = Color(0xFFF2B720);
+                  } else if (index == 1) {
+                    circleColor = Color(0xFF4072AC);
+                  } else if (index == 2) {
+                    circleColor = Color(0xFFDE4C0E);
+                  } else {
+                    circleColor = Color(0xFF9195A4);
+                  }
+
+                  return Padding(
+                    padding: index == numbers.length - 2
+                        ? EdgeInsets.symmetric(horizontal: 2.0)
+                        : EdgeInsets.symmetric(horizontal: 4.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: circleColor,
+                          child: Text(
+                            number == "?" || number == " " ? "" : '$number',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'PyeongChangPeaceLight',
+                            ),
+                          ),
+                        ),
+                        if (index == numbers.length - 2)
+                          Padding(
+                            padding: const EdgeInsets.only(left: 6.0),
+                            child: Icon(Icons.add, size: 40, color: Color(0xFF9195A4)),
+                          ),
+                      ],
+                    ),
+                  );
+                }).toList(),
               ),
               SizedBox(height: 16),
               Text(
-                '2024.07',
+                '$yyyy.$dd',
                 style: TextStyle(
                   fontSize: 18,
-                  fontFamily: 'PretendardMedium',
+                  fontFamily: 'PretendardLight',
                   color: Colors.black,
                 ),
               ),
@@ -78,9 +97,14 @@ class _MyDiaryPageState extends State<MyDiaryPage> {
                   padding: const EdgeInsets.all(12.0),
                   child: Column(
                     children: [
-                      CalendarWidget(),
+                      CalendarWidget(
+                        selectedDate: _selectedDate,
+                        onDateSelected: _selectDate,
+                      ),
                       SizedBox(height: 16),
-                      DiaryEntry(),
+                      // DiaryEntry가 선택된 날짜가 없거나 선택된 날짜가 아닌 경우에는 표시하지 않음
+                      if (_selectedDate != null)
+                        DiaryEntry(date: _selectedDate!),
                     ],
                   ),
                 ),
@@ -95,6 +119,11 @@ class _MyDiaryPageState extends State<MyDiaryPage> {
 }
 
 class CalendarWidget extends StatelessWidget {
+  final int? selectedDate;
+  final void Function(int) onDateSelected;
+
+  CalendarWidget({required this.selectedDate, required this.onDateSelected});
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -103,13 +132,13 @@ class CalendarWidget extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('SUN', style: TextStyle(color: Colors.grey)),
-              Text('MON', style: TextStyle(color: Colors.grey)),
-              Text('TUE', style: TextStyle(color: Colors.grey)),
-              Text('WED', style: TextStyle(color: Colors.grey)),
-              Text('THU', style: TextStyle(color: Colors.grey)),
-              Text('FRI', style: TextStyle(color: Colors.grey)),
-              Text('SAT', style: TextStyle(color: Colors.grey)),
+              Text('SUN', style: TextStyle(color: Colors.grey, fontFamily: 'PretendardMedium')),
+              Text('MON', style: TextStyle(color: Colors.grey, fontFamily: 'PretendardMedium')),
+              Text('TUE', style: TextStyle(color: Colors.grey, fontFamily: 'PretendardMedium')),
+              Text('WED', style: TextStyle(color: Colors.grey, fontFamily: 'PretendardMedium')),
+              Text('THU', style: TextStyle(color: Colors.grey, fontFamily: 'PretendardMedium')),
+              Text('FRI', style: TextStyle(color: Colors.grey, fontFamily: 'PretendardMedium')),
+              Text('SAT', style: TextStyle(color: Colors.grey, fontFamily: 'PretendardMedium')),
             ],
           ),
           SizedBox(height: 8),
@@ -118,18 +147,26 @@ class CalendarWidget extends StatelessWidget {
             crossAxisCount: 7,
             crossAxisSpacing: 4,
             mainAxisSpacing: 4,
-            physics: NeverScrollableScrollPhysics(), 
+            physics: NeverScrollableScrollPhysics(),
             children: List.generate(31, (index) {
-              return Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: index == 30 ? Colors.blueGrey[100] : Colors.transparent,
-                ),
-                child: Center(
-                  child: Text(
-                    '${index + 1}',
-                    style: TextStyle(
-                      color: index == 30 ? Colors.blue : Colors.black,
+              int date = index + 1;
+              bool isSelected = date == selectedDate;
+              return GestureDetector(
+                onTap: () => onDateSelected(date),
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: isSelected ? Color(0xFFD0D4E8) : Colors.transparent,
+                    border: isSelected ? Border.all(color: Color(0xFF454FAF), width: 2) : null,
+                  ),
+                  child: Center(
+                    child: Text(
+                      '$date',
+                      style: TextStyle(
+                        color: isSelected ? Color(0xFF454FAF) : Color(0xFF898989),
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'PyeongChangPeaceLight',
+                      ),
                     ),
                   ),
                 ),
@@ -143,6 +180,10 @@ class CalendarWidget extends StatelessWidget {
 }
 
 class DiaryEntry extends StatelessWidget {
+  final int date;
+
+  DiaryEntry({required this.date});
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -155,10 +196,11 @@ class DiaryEntry extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            '2024년 8월 31일 수요일',
+            '2024년 8월 ${date}일 ${_getDayOfWeek(date)}요일',
             style: TextStyle(
-              fontSize: 14, 
-              color: Color(0xFF454FAF), 
+              fontSize: 14,
+              color: Color(0xFF454FAF),
+              fontFamily: 'PretendardSemiBold',
             ),
           ),
           SizedBox(height: 12),
@@ -166,24 +208,35 @@ class DiaryEntry extends StatelessWidget {
             '로또일기디자인입니다.',
             style: TextStyle(
               fontSize: 22,
-              color: Color(0xFF242424), 
-              fontWeight: FontWeight.bold,
+              color: Color(0xFF242424),
+              fontFamily: 'PretendardBold',
             ),
           ),
-          SizedBox(height: 12),
+          // 실선 추가
+          Divider(
+            color: Colors.grey,
+            thickness: 1,
+            height: 28,
+          ),
           Text(
             '디자인재미있어하지만힘들어하지만재밋어흐베베베베ㅔ베ㅔㅓ라러아렁라어ㅏ어랑ㄹ알아러알아러알알ㅇ라어',
             style: TextStyle(
               fontSize: 18,
-              color: Color(0xFF242424), 
+              color: Color(0xFF242424),
+              fontFamily: 'PretendardMedium',
             ),
           ),
         ],
       ),
     );
   }
+
+  String _getDayOfWeek(int date) {
+    DateTime dateTime = DateTime(2024, 8, date);
+    return ['일', '월', '화', '수', '목', '금', '토'][dateTime.weekday % 7];
+  }
 }
 
 void main() => runApp(MaterialApp(
-      home: MyDiaryPage(),
-    ));
+  home: MyDiaryPage(),
+));
--- lib/Page/writeDiary.dart
+++ lib/Page/writeDiary.dart
@@ -6,9 +6,15 @@ class WritePage extends StatefulWidget {
 }
 
 class _WritePageState extends State<WritePage> {
+  final List<dynamic> numbers = [7, 12, 23, 32, 34, "?", " "];
+  final int yyyy = 2024;
+  final int mm = 8;
+  final int dd = 31;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         leading: Padding(
           padding: const EdgeInsets.only(left: 16.0),
@@ -34,133 +40,134 @@ class _WritePageState extends State<WritePage> {
       ),
       body: Container(
         color: Color(0xFFF3F4F6),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: numbers.asMap().entries.map((entry) {
+                int index = entry.key;
+                dynamic number = entry.value;
+                Color circleColor;
+
+                if (index == 0 || index == numbers.length - 1) {
+                  circleColor = Color(0xFFF2B720);
+                } else if (index == 1) {
+                  circleColor = Color(0xFF4072AC);
+                } else if (index == 2) {
+                  circleColor = Color(0xFFDE4C0E);
+                } else {
+                  circleColor = Color(0xFF9195A4);
+                }
+
+                return Padding(
+                  padding: index == numbers.length - 2
+                      ? EdgeInsets.symmetric(horizontal: 2.0)
+                      : EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: circleColor,
+                        child: Text(
+                          number == " " || number == null ? "" : (number == "?" ? '?' : '$number'),
+                          style: TextStyle(color: Colors.white, fontFamily: 'PyeongChangPeaceLight'),
+                        ),
+                      ),
+                      if (index == numbers.length - 2)
+                        Padding(
+                          padding: const EdgeInsets.only(left: 6.0),
+                          child: Icon(Icons.add, size: 40, color: Color(0xFF9195A4)),
+                        ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '"?"에 번호를 입력해주세요.',
+              style: TextStyle(
+                fontSize: 16,
+                fontFamily: 'PretendardMedium',
+                color: Color(0xFF242424),
+              ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  CircleAvatar(
-                    backgroundColor: Color(0xFFF2B720),
-                    child: Text('7', style: TextStyle(color: Colors.white)),
+                  Text(
+                    '$yyyy년 $mm월 $dd일 수요일',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontFamily: 'PretendardSemiBold',
+                      color: Color(0xFF454FAF),
+                    ),
                   ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFF4072AC),
-                    child: Text('12', style: TextStyle(color: Colors.white)),
+                  SizedBox(height: 16),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: '제목을 입력해주세요.',
+                      hintStyle: TextStyle(
+                        fontSize: 22,
+                        fontFamily: 'PretendardBold',
+                        color: Colors.black,
+                      ),
+                      border: InputBorder.none,
+                    ),
                   ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFFDE4C0E),
-                    child: Text('23', style: TextStyle(color: Colors.white)),
+                  SizedBox(height: 8),
+                  Divider(
+                    color: Color(0xFFDADADA),
+                    thickness: 1,
                   ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFF9195A4),
-                    child: Text('32', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFF9195A4),
-                    child: Text('34', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Colors.grey[400],
-                    child: Text('?', style: TextStyle(color: Colors.white)),
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Color(0xFFF2B720),
-                    child: Icon(Icons.add, color: Colors.white),
+                  SizedBox(height: 8),
+                  TextField(
+                    maxLines: 8,
+                    decoration: InputDecoration(
+                      hintText: '내용을 입력해주세요.',
+                      hintStyle: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'PretendardLight',
+                        color: Colors.grey,
+                      ),
+                      border: InputBorder.none,
+                    ),
                   ),
                 ],
               ),
-              SizedBox(height: 8),
-              Text(
-                '"?"에 번호를 입력해주세요.',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontFamily: 'PretendardMedium',
-                  color: Color(0xFF242424),
-                ),
-              ),
-              SizedBox(height: 16),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '2024년 8월 31일 수요일',
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontFamily: 'PretendardSemiBold',
-                        color: Color(0xFF454FAF),
-                      ),
-                    ),
-                    SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: '제목을 입력해주세요.',
-                        hintStyle: TextStyle(
-                          fontSize: 22,
-                          fontFamily: 'PretendardBold',
-                          color: Colors.black,
-                        ),
-                        border: InputBorder.none,
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Divider(
-                      color: Color(0xFFDADADA), 
-                      thickness: 1,
-                    ),
-                    SizedBox(height: 8),
-                    TextField(
-                      maxLines: 8,
-                      decoration: InputDecoration(
-                        hintText: '내용을 입력해주세요.',
-                        hintStyle: TextStyle(
-                          fontSize: 18,
-                          fontFamily: 'PretendardLight',
-                          color: Colors.grey,
-                        ),
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 28),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFF454FAF),
-                    padding: EdgeInsets.symmetric(horizontal: 115, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+            ),
+            SizedBox(height: 28),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF454FAF),
+                  padding: EdgeInsets.symmetric(horizontal: 115, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
                   ),
-                  child: Text(
-                    '작성 완료',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontFamily: 'PretendardSemiBold',
-                    ),
+                ),
+                child: Text(
+                  '작성 완료',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontFamily: 'PretendardSemiBold',
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
